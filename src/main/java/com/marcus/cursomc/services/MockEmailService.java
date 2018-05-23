1c3982bbe2fe47d2596dd316b231bd814146d44e
Marcus Santos <marcus817@gmail.com>
2018-05-23 20:33:54 -0300

    Implementando o MockEmail com logger, Pattern Strategy e Template Method

---
+++ src/main/java/com/marcus/cursomc/services/MockEmailService.java
@@ -0,0 +1,16 @@
+package com.marcus.cursomc.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
+
+public class MockEmailService extends AbastractEmailService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
+	
+	public void sendEmail(SimpleMailMessage msg) {
+		LOG.info("Simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+	}
+}
